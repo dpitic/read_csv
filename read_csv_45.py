29d6cc2a151245df168dc55cab72bee6d8948d41
Dan Pitic <dpitic@gmail.com>
2018-03-17 16:14:28 +0800

    Added functionality to extract highest frequency.

--- read_csv_45.py
+++ read_csv_45.py
@@ -20,3 +20,17 @@ print('max = {}; number = {}'.format(maximum, n_max))
 minimum = min(freq)
 n_min = freq.index(minimum) + 1
 print('min = {}; number = {}'.format(minimum, n_min))
+# Convert to list of tuples
+lt = [(i+1, freq[i]) for i in range(len(freq))]
+print(lt)
+print('length = {}'.format(len(lt)))
+# Sort list of tuples by frequency
+slt = sorted(lt, key=lambda item: (item[1], item[0]), reverse=True)
+print(slt)
+print('length = {}'.format(len(slt)))
+# First six tuples
+firstsix = [slt[i] for i in range(6)]
+# First six sorted by number
+sortedhighest = sorted(firstsix, key=lambda item: (item[0], item[1]))
+print(sortedhighest)
+
